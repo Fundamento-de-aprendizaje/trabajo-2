1c4ccd1b2a88e035f919b6b563247f6f33ee8421
M-Olson-start <miriamlorenaolson@gmail.com>
2025-05-13 22:43:23 -0300

    últimas modificaciones
    Co-authored-by: Valentino Chiappanni <ValentinoChiappanni@users.noreply.github.com>
    Co-authored-by: rominavera77 <rominavera77@users.noreply.github.com>

--- t2.py
+++ t2.py
@@ -12,38 +12,49 @@ from graphviz import Digraph  # Herramienta para crear gráficos de árboles
 def cargar_datos(url, columnas, codificacion='latin1'):
     """
     Carga un CSV desde una URL y devuelve un DataFrame con las columnas especificadas.
-    """
-    df = pd.read_csv(url, usecols=columnas, encoding=codificacion)  # Carga el archivo CSV con las columnas y codificación especificadas
-    print(f"[cargar_datos] Datos cargados con {len(df)} filas y {len(df.columns)} columnas.")  # Muestra información sobre los datos cargados
+    """  # Carga el archivo CSV con las columnas y codificación especificadas
+    df = pd.read_csv(url, usecols=columnas, encoding=codificacion) 
+         # Muestra información sobre los datos cargados
+    print(f"[cargar_datos] Datos cargados con {len(df)} filas y {len(df.columns)} columnas.")  
     return df  # Devuelve el DataFrame cargado
 
 def filtrar_edad(df, columna_edad, edad_min, edad_max):
     """
     Filtra el DataFrame para incluir solo filas cuya edad esté entre edad_min y edad_max.
-    """
-    filtrado = df[(df[columna_edad] >= edad_min) & (df[columna_edad] <= edad_max)].copy()  # Filtra las filas según el rango de edad
-    print(f"[filtrar_edad] Filtrado: {len(filtrado)} filas entre {edad_min} y {edad_max} años.")  # Muestra el número de filas filtradas
+    """             # Filtra las filas según el rango de edad
+    filtrado = df[(df[columna_edad] >= edad_min) & (df[columna_edad] <= edad_max)].copy() 
+                    # Muestra el número de filas filtradas
+    print(f"[filtrar_edad] Filtrado: {len(filtrado)} filas entre {edad_min} y {edad_max} años.")  
     return filtrado  # Devuelve el DataFrame filtrado
 
 def preprocesar_categoricas(df, columna_target):
     """
     Selecciona solo columnas categóricas y elimina filas sin valor en la variable target.
     """
-    categ = df.select_dtypes(include='object').dropna(subset=[columna_target])  # Selecciona columnas categóricas y elimina filas con valores nulos en la columna objetivo
-    print(f"[preprocesar_categoricas] Columnas categóricas: {list(categ.columns)}. Filas tras dropna: {len(categ)}.")  # Muestra las columnas categóricas y el número de filas restantes
-    return categ  # Devuelve el DataFrame procesado
+    # Selecciona columnas categóricas y elimina filas con valores nulos en la columna objetivo
+    categ = df.select_dtypes(include='object').dropna(subset=[columna_target]) 
+    # Muestra las columnas categóricas y el número de filas restantes 
+    print(f"[preprocesar_categoricas] Columnas categóricas: {list(categ.columns)}. Filas tras dropna: {len(categ)}.")  
+    # Devuelve el DataFrame procesado
+    return categ  
 #################################################### EJERCICIO 1 PUNTO 1  ####################################################################
 
 def dividir_entrenamiento_prueba(df, prueba_size=0.2, random_state=None):
     """
     Mezcla aleatoriamente el DataFrame y lo divide en entrenamiento y prueba según prueba_size.
     """
-    df_shuffled = df.sample(frac=1, random_state=random_state).reset_index(drop=True)  # Mezcla aleatoriamente las filas del DataFrame
-    idx = int(len(df_shuffled)*(1-prueba_size))  # Calcula el índice para dividir el DataFrame
-    entrenamiento = df_shuffled.iloc[:idx]  # Selecciona las filas para el conjunto de entrenamiento
-    prueba  = df_shuffled.iloc[idx:]  # Selecciona las filas para el conjunto de prueba
-    print(f"[dividir_entrenamiento_prueba] Entrenamiento:{len(entrenamiento)} filas, Prueba:{len(prueba)} filas.")  # Muestra el tamaño de los conjuntos
-    return entrenamiento, prueba  # Devuelve los conjuntos de entrenamiento y prueba
+    # Mezcla aleatoriamente las filas del DataFrame
+    df_shuffled = df.sample(frac=1, random_state=random_state).reset_index(drop=True)  
+    # Calcula el índice para dividir el DataFrame
+    idx = int(len(df_shuffled)*(1-prueba_size))  
+    # Selecciona las filas para el conjunto de entrenamiento
+    entrenamiento = df_shuffled.iloc[:idx]  
+    # Selecciona las filas para el conjunto de prueba
+    prueba  = df_shuffled.iloc[idx:]  
+    # Muestra el tamaño de los conjuntos
+    print(f"Entrenamiento:{len(entrenamiento)} filas, Prueba:{len(prueba)} filas.") 
+    # Devuelve los conjuntos de entrenamiento y prueba
+    return entrenamiento, prueba  
 
 #################################################### EJERCICIO 1 PUNTO 2  ####################################################################
 # --- Implementación ID3 ---
@@ -52,20 +63,27 @@ def entropia(serie):
     Calcula la entropía de Shannon de una serie de etiquetas.
     medida cuantitativa de la cantidad de informaci´on que contiene una variable
     """
-    conteos = Counter(serie)  # Cuenta la frecuencia de cada valor en la serie (columna de estado, ACEPTADO o RECHAZADO)
+    # Cuenta la frecuencia de cada valor en la serie (columna de estado, ACEPTADO o RECHAZADO)
+    conteos = Counter(serie)  
     total = len(serie)  # Calcula el número total de elementos
-    resultadoEntropia = -sum((cantEstado/total)*np.log2(cantEstado/total) for cantEstado in conteos.values())  # Calcula la entropía usando la fórmula de Shannon, la cantidad de veces que tenemos por cada estado
-    print(f"[entropia] Valores: {dict(conteos)}, Entropía: {resultadoEntropia:.4f}")  # Muestra los valores y la entropía calculada
-    return resultadoEntropia  # Devuelve la entropía calculada
+    # Calcula la entropía usando la fórmula de Shannon, la cantidad de veces que tenemos por cada estado
+    resEntropia = -sum((cantEstado/total)*np.log2(cantEstado/total) for cantEstado in conteos.values())  
+    # Muestra los valores y la entropía calculada
+    print(f"[entropia] Valores: {dict(conteos)}, Entropía: {resEntropia:.4f}") 
+    return resEntropia  # Devuelve la entropía calculada
 
 def ganancia_informacion(df, atributo, target):
     """
     Calcula la ganancia de información de particionar df por caracteristica.
     """
-    ent_total = entropia(df[target])  # Calcula la entropía total del conjunto de datos
-    ent_ponderada = sum((len(sub)/len(df))*entropia(sub[target]) for _, sub in df.groupby(atributo))  # Calcula la entropía ponderada por partición
-    ganancia = ent_total - ent_ponderada  # Calcula la ganancia de información
-    print(f"[ganancia_informacion] Feature: {atributo}, Ganancia: {ganancia:.4f}")  # Muestra la característica y su ganancia de información
+     # Calcula la entropía total del conjunto de datos
+    ent_total = entropia(df[target]) 
+    # Calcula la entropía ponderada por partición
+    ent_ponderada = sum((len(sub)/len(df))*entropia(sub[target]) for _, sub in df.groupby(atributo))  
+    # Calcula la ganancia de información
+    ganancia = ent_total - ent_ponderada  
+    # Muestra la característica y su ganancia de información
+    print(f"[ganancia_informacion] Feature: {atributo}, Ganancia: {ganancia:.4f}")  
     return ganancia  # Devuelve la ganancia de información
 
 def construir_id3(df, target, atributos):  # caracteristicas son los atributos 
@@ -73,13 +91,16 @@ def construir_id3(df, target, atributos):  # caracteristicas son los atributos
     Construye recursivamente un árbol de decisión usando ID3.
     """
     # Caso base: si todas las etiquetas iguales
-    if len(df[target].unique()) == 1:    # unique unifica para eliminar duplicados y si hay un solo elemento es una hoja o nodo puro
+    # unique unifica para eliminar duplicados y si hay un solo elemento es una hoja o nodo puro
+    if len(df[target].unique()) == 1:    
         clase = df[target].iloc[0]
         print(f"[construir_id3] Nodo hoja con clase {clase}.")
         return clase
     # Sin atributos remanentes
-    if not atributos:           # no tiene valor el atributo en esa fila
-        moda = df[target].mode()[0]   # lo etiqueta con el más frecuente
+    # no tiene valor el atributo en esa fila
+    if not atributos:      
+        # lo etiqueta con el más frecuente     
+        moda = df[target].mode()[0]   
         print(f"[construir_id3] Sin atributos, retorna moda {moda}.")
         return moda
 
@@ -117,20 +138,6 @@ def predecir_id3(arbol, fila, primer_valor_de_moda):
 
 ###### Matriz de Confusión Acurracy F1-score ### EJERCICIO 1 PUNTOS  3 y 4 - EJERCICIO 2 PUNTOS 2 Y 3###############
 # --- Evaluación de Modelos ---
-
-# def evaluar(y_true, y_pred):
-#     """
-#     Calcula matriz de confusión y métricas (accuracy, precision, recall, F1).
-#     """
-#     etiquetas = sorted(set(y_true))
-#     cm = confusion_matrix(y_true, y_pred, labels=etiquetas)
-#     acc = accuracy_score(y_true, y_pred)
-#     prec = precision_score(y_true, y_pred, pos_label=etiquetas[0])
-#     rec = recall_score(y_true, y_pred, pos_label=etiquetas[0])
-#     f1 = f1_score(y_true, y_pred, pos_label=etiquetas[0])
-#     print(f"[evaluar] CM:\n _Matriz de Confusión {cm}\nAccuracy: {acc:.4f}, Precision: {prec:.4f}, Recall: {rec:.4f}, F1: {f1:.4f}")
-#     return {'cm': cm, 'accuracy': acc, 'precision': prec, 'recall': rec, 'f1': f1}
-
 
 def evaluar(y_true, y_pred):
     """
@@ -198,7 +205,7 @@ def graficar_curva_precision(X_train, y_train, X_test, y_test, max_arboles=10):
     """
     arboles = list(range(1, max_arboles+1))
     p_train, p_test = [], []
-    etiqueta = y_train.mode()[0]
+    etiqueta = y_train.mode()[0] 
     for n in arboles:
         rf = RandomForestClassifier(n_estimators=n, random_state=None)
         rf.fit(X_train, y_train)
